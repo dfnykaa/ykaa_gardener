9a90f1ad7f5badf12bc51df4682bf0fe4882ac8b
ykaa <pepahurka3@gmail.com>
2026-04-20 15:29:07 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,17 +1,15 @@
 Config = {}
 
-Config.Debug = false
-Config.ProgressTime = 5000
 
 Config.Gardener = {
     Ped = {
         model = "s_m_m_cntrybar_01",
-        coords = vec4(1219.8287, -1038.8408, 8.3379, 297.6415),
+        coords = vec4(-1219.8287, -1038.8408, 8.3379, 297.6415),
     },
     Vehicle = {
         enabled = true,
         model = "burrito",
-        spawnCoords = vec4(1228.2515, -1029.4132, 8.2507, 105.7099)
+        spawnCoords = vec4(-1228.2515, -1029.4132, 8.2507, 105.7099)
     },
     Blip = {
         sprite = 85,
@@ -22,12 +20,6 @@ Config.Gardener = {
 }
 
 Config.Locations = {
-    { 
-        coords = vector4(971.0568, -1276.2633, 5.8042, 209.8318), 
-    },
-    {
-        coords = vector4(617.0525, 10.3911, 41.8292, 275.2220),
-    },
     { 
         coords = vector4(1147.5624, -529.8224, 64.4757, 11.8456), 
     },
@@ -41,12 +33,12 @@ Config.Locations = {
         coords = vector4(940.6877, -290.9383, 66.8206, 206.7081), 
     },
     { 
-        coords = vector4(930.1707, 271.2058, 70.5605, 242.9916), 
+        coords = vector4(-831.1078, 828.3339, 199.2278, 104.6165), 
     },
     { 
-        coords = vector4(789.9337, 844.6265, 204.9641, 62.5976), 
+        coords = vector4(-1161.4777, -247.3931, 37.7272, 303.4427), 
     },
     { 
-        coords = vector4(1507.8898, -495.9486, 35.5033, 306.8759), 
+        coords = vector4(-928.9522, 271.4176, 70.5699, 27.2087), 
     },
 }
